bdacabffc4b1c38c54e1d88021aa3daa78fb3ec6
j1r1k <jiri.marsicek@gmail.com>
2016-01-11 19:29:18 +0100

    Cleanup

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -9,16 +9,16 @@ function pwds () {
 
   PRE=""
   NAME="${1}"
-  
+
   if [[ "${NAME}" != "${NAME#${HOME}/}" || -z "${NAME#${HOME}}" ]]
   then
-    PRE+='~' NAME="${NAME#${HOME}}" LENGTH=$[LENGTH-1]
+    PRE+='~' NAME="${NAME#${HOME}}" LENGTH=$((LENGTH-1))
   fi
-  if ((${#NAME}>${LENGTH}))
+  if ((${#NAME}>LENGTH))
   then 
-    NAME="/${PWDS_SYMBOL}${NAME:$[${#NAME}-LENGTH+1]}"
+    NAME="/${PWDS_SYMBOL}${NAME:$((${#NAME}-LENGTH+1))}"
   fi
-  
+
   echo "${PRE}${NAME}" 
 
   unset LENGTH PRE NAME
@@ -27,7 +27,7 @@ function pwds () {
 function git-check () {
 
   [ -x "$(which git)" ] || exit
-  
+
   # get current branch name or short SHA1 hash for detached head
   BRANCH="$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
   [ -n "${BRANCH}" ] || exit
@@ -43,26 +43,25 @@ function git-info () {
   git-check
 
   if [ "$(git rev-parse --is-bare-repository)" = "true" ]; then
-    printf "[bare]"
+    echo -n "bare"
     return
   fi
- 
+
   MARKS=""
-  
+
   # branch is modified?
   [ -n "$(git status --porcelain)" ] && MARKS+=" ${GIT_BRANCH_CHANGED_SYMBOL}"
-  
+
   # how many commits local branch is ahead/behind of remote?
-  STAT="$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
   AHEADN="$(git status --porcelain --branch | sed -n 's~^\#\#.*ahead \([0-9]*\).*~\1~p')"
   BEHINDN="$(git status --porcelain --branch | sed -n 's~^\#\#.*behind \([0-9]*\).*~\1~p')"
   [ -n "${AHEADN}" ] && MARKS+=" ${GIT_NEED_PUSH_SYMBOL}${AHEADN}"
   [ -n "${BEHINDN}" ] && MARKS+=" ${GIT_NEED_PULL_SYMBOL}${BEHINDN}"
-  
+
   # print the git branch segment without a trailing newline
   printf "${GIT_BRANCH_SYMBOL}${BRANCH}${MARKS}"
 
-  unset MARKS STAT AHEADN BEHINDN
+  unset MARKS AHEADN BEHINDN
 }
 
 FG_LRED='\[\e[91m\]'
@@ -76,8 +75,8 @@ BG_LBLU='\[\e[104m\]'
 BG_WHT='\[\e[107m\]'
 
 RESET='\[\e[0m\]'
-INVERT='\[\e[7m\]'
-BOLD='\[\e[1m\]'
+#INVERT='\[\e[7m\]'
+#BOLD='\[\e[1m\]'
 
 PS1=""
 
